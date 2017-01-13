90262f60c18239f2c0e7230193f716cafae0bf0c
petruninevgeniy <petruninevgeniy@gmail.com>
2017-01-13 16:25:13 +0200

    add animation to hisrory page

--- app/js/jquery.animation.js
+++ app/js/jquery.animation.js
@@ -36,7 +36,7 @@
                     var curItem = $(this),
                         topPos = _obj.offset().top;
 
-                    if( _window.scrollTop() > (topPos - windowH/1.8) && !curItem.hasClass( 'animation' ) ){
+                    if( _window.scrollTop() > (topPos - windowH/1.3) && !curItem.hasClass( 'animation' ) ){
 
                         curItem.addClass( 'animation' );
 
